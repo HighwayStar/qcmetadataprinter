4d9e4e2ef9c665754b3b7a2db8ea646a49d0c709
Demon000 <demonsingur@gmail.com>
2018-01-15 21:44:45 +0200

    get_offsets: clean up

--- get_offsets.c
+++ get_offsets.c
@@ -6,14 +6,14 @@
 #define CAM_LIB_OEM "liboemcamera.so"
 
 #define PRINT(PARAM_ID, table_ptr)  \
-	blob_pointer = (char*)(get_pointer_of(PARAM_ID, table_ptr))-(char*)(table_ptr); \
-	oss_pointer = (char*)(POINTER_OF_META(PARAM_ID, table_ptr))-(char*)(table_ptr); \
-	blob_size=get_size_of(PARAM_ID); \
-	oss_size=SIZE_OF_PARAM(PARAM_ID, table_ptr); \
-	if ((blob_pointer != oss_pointer) || (blob_size != oss_size) ) \
-		printf("FIXME!!!\n"); \
-	printf("BLOB: %s index=%d pointer=%d size=%d\n", #PARAM_ID, PARAM_ID, blob_pointer, blob_size); \
-        printf("OSS : %s index=%d pointer=%d size=%d\n", #PARAM_ID, PARAM_ID, oss_pointer, oss_size); 
+    blob_pointer = (char*)(get_pointer_of(PARAM_ID, table_ptr)) - (char*)(table_ptr); \
+    oss_pointer = (char*)(POINTER_OF_META(PARAM_ID, table_ptr)) - (char*)(table_ptr); \
+    blob_size = get_size_of(PARAM_ID); \
+    oss_size = SIZE_OF_PARAM(PARAM_ID, table_ptr); \
+    if ((blob_pointer != oss_pointer) || (blob_size != oss_size) ) \
+        printf("FIXME!!!\n"); \
+    printf("BLOB: %s index=%d pointer=%d size=%d\n", #PARAM_ID, PARAM_ID, blob_pointer, blob_size); \
+    printf("OSS : %s index=%d pointer=%d size=%d\n", #PARAM_ID, PARAM_ID, oss_pointer, oss_size);
 
 
 typedef void* (*get_pointer_of_t)(cam_intf_parm_type_t, metadata_buffer_t*);
@@ -51,15 +51,17 @@ int main() {
         fprintf(stderr, "%s\n", dlerror());
         return 1;
     }
+
     get_size_of = (get_size_of_t) dlsym(handle, "get_size_of");
     if (!get_size_of)  {
         fprintf(stderr, "%s\n", dlerror());
         return 1;
     }
-    int blob_pointer=0;
-    int oss_pointer=0;
-    int blob_size=0;
-    int oss_size=0;
+
+    int blob_pointer = 0;
+    int oss_pointer = 0;
+    int blob_size = 0;
+    int oss_size = 0;
 
     /* INCLUDE() macros from metadata_data_t go after this */
 
